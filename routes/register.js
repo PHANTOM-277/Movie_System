834db1e2dce3ad2228c897d7e8299d826e09bf59
arnav <arnav.mathur.1024@gmail.com>
2025-03-06 19:24:47 +0530

    made registeration end point

--- routes/register.js
+++ routes/register.js
@@ -1,3 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const router = express.Router();
+const User = require('./schema/user.js');
 
+router.post('/register/user', async(req,res)=>{
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
+        if(!email || !password){
+            //if data is not recieved
+            return res.status(400).json({msg:"Email or Passwrod not received"});
+        }
+        const account = await User.findOne({email:email});
+        if(account){
+            //if an account with given email already exists.
+            return res.status(400).json({msg:"An account with the given email already exists. Please choose a different username"});
+        }
+        //if we have reached till here then we can register
+        //now we should hash the password and store it
+        const salt = await bcrypt.genSalt(10);
+        hashed_password = await bcrypt.hash(password, salt);
+        await User.create({
+            email:email,
+            password:hashed_password
+        });
+        return res.status(201).json({msg:"Registered User"});
+    }
+    catch(e){
+        console.log(`Error in user registeration : ${e}`);
+        return res.status(500).json({msg:"Server Error"});
+    }
+});
