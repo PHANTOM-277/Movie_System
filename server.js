3f2750e407f0b3e805b3525d5b8768588e8ec1dc
arnav <arnav.mathur.1024@gmail.com>
2025-03-06 19:00:59 +0530

    added json parsing middleware and comments

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
+/* import libraries*/
 const express = require('express');
 const mongoose = require('mongoose');
 
+/* import routes */
+
+/* get environment variables*/
 const port = process.env.PORT;
 const connection_string = process.env.DBSTRING;
 
+/* connect to mongodb*/
 mongoose.connect(connection_string)
     .then(()=>console.log("Connected to Database"))
     .catch((e)=>console.log(`Error in connecting to database : ${e}`))
 
 const app = express();
+
+/* middleware*/
+app.use(express.json());
+
+/* routes */
+
 
 app.listen(port, ()=>console.log(`Up and running on port : ${port}`));
