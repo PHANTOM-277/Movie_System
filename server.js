f3ff7dc61a666916174ef2543612ee986d2c0e03
arnav <arnav.mathur.1024@gmail.com>
2025-03-06 18:57:30 +0530

    connecting to database

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const port = process.env.PORT;
 const connection_string = process.env.DBSTRING;
 
+mongoose.connect(connection_string)
+    .then(()=>console.log("Connected to Database"))
+    .catch((e)=>console.log(`Error in connecting to database : ${e}`))
+
 const app = express();
 
 app.listen(port, ()=>console.log(`Up and running on port : ${port}`));
