7fab5820ceaae9932a1373b7205deb522d81f65d
arnav <arnav.mathur.1024@gmail.com>
2025-03-07 18:41:26 +0530

    made the route for admins to add new movies

--- routes/movies.js
+++ routes/movies.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.get('/info', async(req,res)=>{
     try{
-        const movies = Movie.find({date:{$gte:new Date()}});
-        if(movies.length === 0 || !movies){
+        const movies = await Movie.find({date:{$gte:new Date()}});
+        if(movies.length === 0){
             //if no movies are scheduled
             return res.status(404).json({msg:"No movies scheduled"});
         }
@@ -23,8 +23,43 @@ router.get('/info', async(req,res)=>{
 router.post('/admin/new_movie', authenticate(1), async(req,res)=>{
     //this route is only for admin , hence the authenticate(1)
     try{
-        //read data from body
-        return res.status(201).json({msg:"done"});
+        const { movie_name , movie_date , movie_capacity, movie_base_price } = req.body;
+        if(!movie_name || !movie_date || !movie_capacity || !movie_base_price){
+            //not enough information
+            return res.status(400).json({msg:"Missing Information , please give movie name, date, capacity and base price."});
+        }
+        const parsed_movie_date = new Date(movie_date);//parse the date given into Date object
+        if (isNaN(parsedDate.getTime())){
+            //if invalid date
+            return res.status(400).json({ msg: "Invalid date format." });
+        }
+        if (movie_date < new Date()){
+            //if this movie date is before now
+            return res.status(400).json({ msg: "Movie date must be after current date." });
+        }
+        if(movie_capacity<= 0){
+            //invalid capacity
+            return res.status(400).json({msg:"Movie capacity should be more than 0."});
+        }
+        if(movie_base_price <= 0){
+            //invalid base price
+            return res.status(400).json({msg:"Movie base price should be more than 0."});
+        }
+        if(await Movie.findOne({name:movie_name})){
+            //if a movie with this name alreay exists , dont create new one
+            return res.status(400).json({msg:"A movie with this name already exists."})
+        }
+        //if we have reached here , we can create the movie document
+        const movie = new Movie({
+            name:movie_name,
+            date:parsed_movie_date,
+            capacity:movie_capacity,
+            seatsbooked:0, //initially 0
+            base_price:movie_base_price
+        });
+        await movie.save();
+
+        return res.status(201).json({msg:"created movie.",movie:movie});
     }
     catch(e){
         console.log(`Error in adding new movie : ${e}`);
